11dfd6798b5cb574b0c40cbc90cfa43f78d86029
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-03-02 22:13:37 -0300

    implementando componente login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../auth/auth.service';
+import {TokenStorageService} from '../auth/token-storage.service';
+import {LoginRequest} from '../models/request/login.request';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: any = {};
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
+  private loginRequest: LoginRequest;
+
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
 
   ngOnInit() {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getAuthorities();
+    }
   }
 
+  onSubmit() {
+    console.log(this.form);
+
+    this.loginRequest = new LoginRequest(
+      this.form.username,
+      this.form.password);
+
+    this.authService.attemptAuth(this.loginRequest).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUsername(data.username);
+        this.tokenStorage.saveAuthorities(data.authorities);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenStorage.getAuthorities();
+        this.reloadPage();
+      },
+      error => {
+        console.log(error);
+        this.errorMessage = error.error.message;
+        this.isLoginFailed = true;
+      }
+    );
+  }
+
+  reloadPage() {
+    window.location.reload();
+  }
 }
--- src/app/models/request/login.request.ts
+++ src/app/models/request/login.request.ts
@@ -2,4 +2,9 @@
 export class LoginRequest {
   nickname: String;
   password: String;
+
+  constructor(nickname: String, password: String) {
+    this.nickname = nickname;
+    this.password = password;
+  }
 }
