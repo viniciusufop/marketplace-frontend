83698d6d71cbc7c6bcaf3f6596fabdff1482694c
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-03-02 21:50:44 -0300

    implementando o servico de usuario

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:9000';
+  private userUrl = this.baseUrl + '/api/test/user';
+  private pmUrl = this.baseUrl + '/api/test/pm';
+  private adminUrl = this.baseUrl + '/api/test/admin';
+
+  constructor(private http: HttpClient) { }
+
+  getUserBoard(): Observable<string> {
+    return this.http.get(this.userUrl, { responseType: 'text' });
+  }
+
+  getPMBoard(): Observable<string> {
+    return this.http.get(this.pmUrl, { responseType: 'text' });
+  }
+
+  getAdminBoard(): Observable<string> {
+    return this.http.get(this.adminUrl, { responseType: 'text' });
+  }
 }
