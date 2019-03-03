2d2a01c146cd66e364bc2884c3b0b1e4929d3c36
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-03-02 21:43:59 -0300

    implementando o servico para autenticar e para criar usuario

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {SigupRequest} from '../models/request';
+import {SignupRequest} from '../models/request/sigup.request';
+import {LoginRequest} from '../models/request/login.request';
+import {JwtResponse} from '../models/response/jwt.response';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080';
+  private loginUrl = this.baseUrl + '/api/auth/signin';
+  private signupUrl = this.baseUrl + '/api/auth/signup';
+
+  constructor(private http: HttpClient) {}
+
+  attemptAuth(credentials: LoginRequest): Observable<JwtResponse> {
+    return this.http.post<JwtResponse>(this.loginUrl, credentials, httpOptions);
+  }
+
+  signUp(info: SignupRequest): Observable<string> {
+    return this.http.post<string>(this.signupUrl, info, httpOptions);
+  }
 }
--- src/app/models/request/sigup.request.ts
+++ src/app/models/request/sigup.request.ts
@@ -1,5 +1,5 @@
 
-export class SigupRequest {
+export class SignupRequest {
   name: string;
   username: string;
   email: string;
