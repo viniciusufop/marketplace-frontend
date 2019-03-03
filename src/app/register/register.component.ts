c321e6705b8699736b04d9e9cc3d6d76e16a6af5
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-03-02 22:16:08 -0300

    implementando componente registro

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {SignupRequest} from '../models/request/sigup.request';
+import {AuthService} from '../auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: any = {};
+  signupRequest: SignupRequest;
+  isSignedUp = false;
+  isSignUpFailed = false;
+  errorMessage = '';
 
-  ngOnInit() {
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() { }
+
+  onSubmit() {
+    console.log(this.form);
+
+    this.signupRequest = new SignupRequest(
+      this.form.name,
+      this.form.username,
+      this.form.email,
+      this.form.password);
+
+    this.authService.signUp(this.signupRequest).subscribe(
+      data => {
+        console.log(data);
+        this.isSignedUp = true;
+        this.isSignUpFailed = false;
+      },
+      error => {
+        console.log(error);
+        this.errorMessage = error.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
   }
-
 }
