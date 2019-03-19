ef76a61af1eecea22e6f305b4c2960b34d916be0
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-03-19 00:13:22 -0300

    acertos na tela de descrição de produto

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -11,16 +11,22 @@ import {ActivatedRoute} from '@angular/router';
 export class ProductDetailComponent implements OnInit {
 
   product: Product;
-
+  listProductImages: string[] = [];
   constructor(private productsService: ProductsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.productsService.getProduct(this.route.snapshot.params['id']).subscribe(
       data => {
         this.product = data;
+        console.log(this.product.urlPrimaryImage);
+        console.log(this.product.urlImages);
+        this.listProductImages = this.listProductImages.concat(this.product.urlPrimaryImage);
+        this.listProductImages = this.listProductImages.concat(this.product.urlImages);
+        console.log(this.listProductImages);
       }, error => {
         console.log(error);
         this.product = null;
+        this.listProductImages = [];
       }
     );
   }
